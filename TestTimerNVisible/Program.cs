faeb576072ae8010e52de9ace539691079e0a7ff
Kerim Dinçer <kerim_dincer@hotmail.com>
2020-02-28 10:51:57 +0300

    comment

--- TestTimerNVisible/Program.cs
+++ TestTimerNVisible/Program.cs
@@ -8,7 +8,7 @@ namespace TestTimerNVisible
 {
     public static class Program
     {
-
+        //Console u saklamak için DLL'leri ımport edip tanımlamaları yapıyoruz.
         [DllImport("kernel32.dll")]
         static extern IntPtr GetConsoleWindow();
 
@@ -19,42 +19,45 @@ namespace TestTimerNVisible
         //static int SW_SHOW = 5;
         static void Main(string[] args)
         {
-            // Xd
+            //Timer tanımlıyoruz.(10000 = 10 saniye)
             Timer t = new Timer(TimerCallback, null, 0, 10000);
+            //açık consolu değişkene atıyoruz.
             IntPtr myWindow = GetConsoleWindow();
-            // Hide
+            //console ekranının saklanmasını sağlıyoruz.
             ShowWindow(myWindow, SW_HIDE);
             Console.ReadLine();
-            // Show
+            //console ekranını geri göstermek için kullanılıyor.
             //ShowWindow(handle, SW_SHOW);
         }
 
         private static void TimerCallback(Object o)
         {
+            //timer sonucunu console yazdırmamızı sağlıyor.
             Console.WriteLine("Timer: " + DateTime.Now);
             GC.Collect();
+            //hata mesajını metoda göndererek çalıştıyoruz
             WriteToFile("hata");
         }
         private static void WriteToFile(string text)
         {
-
+            //Log klasörünün yollarını belirtiyoruz.
             string root = @"C:\Log";
             string subdir = @"C:\Log\Log-" + DateTime.Now.ToString("dd/MM/yyyy") + ".txt";
 
+            //belirtilen yolda Log klasörü yoksa oluştur.
             if (!Directory.Exists(root))
             {
                 Directory.CreateDirectory(root);
             }
-
+            //belirtilen yolda  log.txt yoksa oluştur.
             if (!Directory.Exists(subdir))
             {
                 FileStream fs = new FileStream(subdir, FileMode.OpenOrCreate, FileAccess.Write);
                 fs.Close();
             }
+            //oluşturulmuş olan log.txt ye satır satır mesajı ve tarih saati yazdır.
             File.AppendAllText(subdir, string.Format(text + " - " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + Environment.NewLine));
         }
-
-
 
     }
 }
