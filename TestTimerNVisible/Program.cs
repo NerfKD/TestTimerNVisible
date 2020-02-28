d291c7309a8914d60bf34267d90175b7647692b3
Kerim Dinçer <kerim_dincer@hotmail.com>
2020-02-28 10:38:57 +0300

    Add project files.

---
+++ TestTimerNVisible/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace TestTimerNVisible
+{
+    public static class Program
+    {
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        static int SW_HIDE = 0;
+        //static int SW_SHOW = 5;
+        static void Main(string[] args)
+        {
+            Timer t = new Timer(TimerCallback, null, 0, 10000);
+            IntPtr myWindow = GetConsoleWindow();
+            // Hide
+            ShowWindow(myWindow, SW_HIDE);
+            Console.ReadLine();
+            // Show
+            //ShowWindow(handle, SW_SHOW);
+        }
+
+        private static void TimerCallback(Object o)
+        {
+            Console.WriteLine("Timer: " + DateTime.Now);
+            GC.Collect();
+            WriteToFile("hata");
+        }
+        private static void WriteToFile(string text)
+        {
+
+            string root = @"C:\Log";
+            string subdir = @"C:\Log\Log-" + DateTime.Now.ToString("dd/MM/yyyy") + ".txt";
+
+            if (!Directory.Exists(root))
+            {
+                Directory.CreateDirectory(root);
+            }
+
+            if (!Directory.Exists(subdir))
+            {
+                FileStream fs = new FileStream(subdir, FileMode.OpenOrCreate, FileAccess.Write);
+                fs.Close();
+            }
+            File.AppendAllText(subdir, string.Format(text + " - " + DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss") + Environment.NewLine));
+        }
+
+
+
+    }
+}
